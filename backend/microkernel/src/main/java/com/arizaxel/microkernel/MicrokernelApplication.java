20f6d9af9d84864512de26116eed4ad334af50e3
arizaxel16 <arizaxel16@gmail.com>
2024-10-26 12:06:00 -0500

    Initial project setup
    react frontend
    spring boot backend

---
+++ backend/microkernel/src/main/java/com/arizaxel/microkernel/MicrokernelApplication.java
@@ -0,0 +1,13 @@
+package com.arizaxel.microkernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrokernelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrokernelApplication.class, args);
+	}
+
+}
---
+++ backend/microkernel/src/test/java/com/arizaxel/microkernel/MicrokernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arizaxel.microkernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrokernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
